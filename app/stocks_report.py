abf30a16d87eea4baa5ebb24c4eab1ce9ec1e7af
stephaniewang1018 <sw1285@georgetown.edu>
2024-11-15 21:33:37 -0500

    Update stocks_report.py

--- app/stocks_report.py
+++ app/stocks_report.py
@@ -12,7 +12,7 @@ load_dotenv() #looks in the ".env" file for env vars
 #
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
-def fetch_stocks_csv():
+def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
     df = read_csv(request_url)
     return df
